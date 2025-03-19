99e0c09541ff959136204b5a01bbf4c2b845bc2c
Marlon Mullet <marlonmullet10@gmail.com>
2025-03-19 19:33:42 +0800

    dean folder

---
+++ dean/include/header.php
@@ -0,0 +1,37 @@
+<?php
+require '../../admin/config/func.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../../admin/admin/assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../../admin/admin/assets/img/favicon.png">
+  <title>
+  PLMun Alumni Tracer
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="../../admin/admin/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../../admin/admin/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="../../admin/admin/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+   <link  href="../../users/Style/prof.css" rel="stylesheet" />
+  <link id="pagestyle" href="assets/css/soft-ui-dashboard.css?v=1.0.7" rel="stylesheet" />
+</head>
+
+<body class="g-sidenav-show  bg-gray-100">
+
+    <?php include ('sidebar.php') ?>
+
+        <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+
+            <?php include ('navbar.php') ?>
+
+                <div class="container-fluid py-4">
